4bbcae60097c8e295c8a47d19385d6aa93018333
Sam <sambolton.kw@gmail.com>
2025-01-07 22:21:15 -0500

    images

---
+++ assets/js/images.js
@@ -0,0 +1,10 @@
+export const images = [
+    {"147": "assets/images/9BBuck.jpg"},
+    {"144": "assets/images/6CBuckDalton.jpg"},
+    {"145": "assets/images/7ABuck.jpg"},
+    {"110": "assets/images/6CMonster.jpg"},
+    {"96": "assets/images/15BMaybeBuck.jpg"},
+    {"86": "assets/images/7BBuck.jpg"},
+    {"44": "assets/images/16EBuck.jpg"},
+    {"94": "assets/images/9BBuck1.jpg"},
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,19 @@ import { huntingAreas } from "./huntingAreas.js";
 import HarvestManager from "./harvestManager.js";
 import MapManager from "./mapManager.js";
 import { rawQuanticoHarvest, keys } from "./harvestData.js";
+import { images } from "./images.js";
 
 const sidebarContent = document.getElementById("sidebar-content");
 
 // Create a HarvestManager instance
 const harvestManager = new HarvestManager(rawQuanticoHarvest, keys);
 
-harvestManager.addImageToSpecificId("147", "assets/images/9BBuck.jpg");
-harvestManager.addImageToSpecificId("144", "assets/images/6CBuckDalton.jpg");
-harvestManager.addImageToSpecificId("145", "assets/images/7ABuck.jpg");
-harvestManager.addImageToSpecificId("110", "assets/images/6CMonster.jpg");
-harvestManager.addImageToSpecificId("96", "assets/images/15BMaybeBuck.jpg");
-harvestManager.addImageToSpecificId("86", "assets/images/7BBuck.jpg");
-harvestManager.addImageToSpecificId("44", "assets/images/16EBuck.jpg");
-harvestManager.addImageToSpecificId("94", "assets/images/9BBuck1.jpg");
+//iterate through images and add them to each id
+images.forEach(img => {
+  const [key, value] = Object.entries(img)[0];
+  console.log("Test", key,value);
+  harvestManager.addImageToSpecificId(key, value)
+})
 
 // Initialize the map using MapManager
 const mapManager = new MapManager(
